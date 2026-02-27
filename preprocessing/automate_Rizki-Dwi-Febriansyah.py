a17137ddea92ee0406b1758f5d4cc339c16eedba
Rizki Dwi Febriansyah <rizkidwifrb@gmail.com>
2026-02-27 15:08:20 +0000

    fix path workflow

--- preprocessing/automate_Rizki-Dwi-Febriansyah.py
+++ preprocessing/automate_Rizki-Dwi-Febriansyah.py
@@ -1,64 +1,52 @@
 import pandas as pd
 import os
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import StandardScaler
 
-# LOAD DATA
 def load_data(path):
-    df = pd.read_csv(path)
-    return df
+    return pd.read_csv(path)
 
 
-# PREPROCESSING
 def preprocessing(df):
 
-    # Handle missing value
     df.fillna(df.mean(numeric_only=True), inplace=True)
 
-    # Encoding categorical column
     le = LabelEncoder()
     cat_cols = df.select_dtypes(include='object').columns
 
     for col in cat_cols:
         df[col] = le.fit_transform(df[col])
 
-    # Scaling numerical data
     scaler = StandardScaler()
     num_cols = df.select_dtypes(include='number').columns
-
     df[num_cols] = scaler.fit_transform(df[num_cols])
 
     return df
 
 
-# SAVE DATA
 def save_data(df, output_path):
-
-    os.makedirs(os.path.dirname(output_path),
-                exist_ok=True)
-
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
     df.to_csv(output_path, index=False)
 
 
-# MAIN PIPELINE
 def main():
 
-    input_path = "dataset_raw/data.csv"
-    df = pd.read_csv(input_path)
+    BASE_DIR = os.getcwd()
 
-    print(df.head())
-    output_path = "dataset_preprocessing/data_clean.csv"
+    input_path = os.path.join(BASE_DIR,
+                              "dataset_raw",
+                              "data.csv")
+
+    output_path = os.path.join(BASE_DIR,
+                               "preprocessing",
+                               "dataset_preprocessing",
+                               "data_clean.csv")
 
     df = load_data(input_path)
-
     df_clean = preprocessing(df)
-
     save_data(df_clean, output_path)
 
-    print("✅ Preprocessing selesai!")
-    print(f"Dataset tersimpan di: {output_path}")
-    print("Workflow preprocessing berhasil dijalankan!")
+    print("✅ Workflow preprocessing berhasil!")
 
 
 if __name__ == "__main__":
